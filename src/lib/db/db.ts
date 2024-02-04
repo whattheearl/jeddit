1b8598b5742e01a2058b55ef05994d6e9fe035a3
jonathan earl <jonathan.earl@tylertech.com>
2024-02-04 12:56:36 -0800

    fix db connection

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,11 +1,14 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
+import { env } from '$env/dynamic/private';
+
+
 export const connection = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: env.DB_HOST,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
   multipleStatements: true,
 });
 export const db = drizzle(connection, { schema: schema, mode: 'default' });
