d8dfb158d8f4606dc3a8c7bb8a165d74e1cda7f0
jonathan earl <jonathan.earl@tylertech.com>
2024-04-21 22:20:59 -0700

    fix: set display name

--- src/routes/(root)/settings/account/+page.server.ts
+++ src/routes/(root)/settings/account/+page.server.ts
@@ -2,7 +2,10 @@ import { error, redirect } from "@sveltejs/kit";
 import type { PageServerLoad, Actions } from "./$types";
 import { base } from "$app/paths";
 import { Database } from 'bun:sqlite';
+import { Logger } from "$lib/logger";
+
 const db = new Database('./db.sqlite');
+const logger = Logger('settings.account')
 
 export const load: PageServerLoad = async ({ cookies }) => {
   const sid = cookies.get('sid') as string;
@@ -10,10 +13,8 @@ export const load: PageServerLoad = async ({ cookies }) => {
   if (!session)
     return redirect(302, '/');
 
-  const user = db.query('SELECT * FROM users WHERE id = $id').get({
-    $id: session.user_id
-  });
-
+  const user = db.query('SELECT * FROM users WHERE id = $id').get({ $id: session.user_id }) as any;
+  logger.info('user', { ...user, picture: null });
   return { user };
 }
 
@@ -23,33 +24,32 @@ export const actions: Actions = {
   default: async ({ cookies, request }) => {
     const sid = cookies.get('sid') as string;
     const session = db.query('SELECT * FROM sessions WHERE id = $id').get({ $id: sid }) as any;
-    console.log({session})
+    console.log({ session })
     if (!session)
       return redirect(302, '/');
 
-    let user = db.query('SELECT * FROM users WHERE id=$id').get({$id: session.user_id}) as any
-    console.log({user})
+    let user = db.query('SELECT * FROM users WHERE id=$id').get({ $id: session.user_id }) as any
+    if (user.name_finalized)
+      return error(400, 'name is already set');
+
     if (!user)
       return redirect(302, `${base}/`);
 
     const formData = await request.formData();
-    const username = formData.get('username');
-    if (!username || !username.toString())
-      error(400, 'Username is required');
-    console.log({username})
-    console.log({user})
-    const existingUser = db.query('SELECT id, username FROM users WHERE username = $username').get({
-      $username: username.toString(),
+    const name = formData.get('name');
+    if (!name || !name.toString())
+      error(400, 'name is required');
+
+    const existingUser = db.query('SELECT id FROM users WHERE name = $name').get({
+      $name: name.toString(),
     });
-
-    console.log({existingUser})
     if (existingUser)
-      error(400, 'Username is already taken');
+      error(400, 'name is already taken');
 
-    user.username = username.toString();
-    db.run('UPDATE users SET username=? WHERE id=?', [username.toString(), user.id]);
-    user = db.query('SELECT * FROM users WHERE id=$id').get({$id: session.user_id}) as any
-    console.log({user})
+    user.name = name.toString();
+    db.run('UPDATE users SET name=?, name_finalized=1 WHERE id=?', [name.toString(), user.id]);
+    user = db.query('SELECT * FROM users WHERE id=$id').get({ $id: session.user_id }) as any
+
     return {
       user
     };
