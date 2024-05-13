0adf060b2519f01e5b7b2cedfcfd2f1d6c65e62c
jonathan earl <jonathan.earl@tylertech.com>
2024-05-12 18:49:00 -0700

    fix(deploy): test

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,8 @@
 VERSION=${1:-"latest"}
+USER=whattheearl
+PROJECT=jeddit
 
-docker build -t ghcr.io/whattheearl/jeddit:latest -t ghcr.io/whattheearl/jeddit:$VERSION .
-echo $GH_TOKEN | docker login ghcr.io -u whattheearl --password-stdin
-docker push ghcr.io/whattheearl/jeddit --all-tags
+docker build -t ghcr.io/$USER/jeddit:latest -t ghcr.io/$USER/$PROJECT:$VERSION .
+echo $GH_TOKEN | docker login ghcr.io -u $USER --password-stdin
+docker push ghcr.io/$USER/$PROJECT:$VERSION
+docker push ghcr.io/$USER/$PROJECT:latest
