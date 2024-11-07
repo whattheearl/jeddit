6321e3885ff0fd24564a8b4a64b134cde960e7a6
Jonathan Earl <jonathan.earl@tylertech.com>
2024-11-07 08:03:37 -0800

    upgrade svelte5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,46 +14,44 @@
 <div class="p-4 flex flex-col items-center md:max-w-[756px] mx-auto">
     <main style="width: 100%;">
         {#each data.posts as p}
-            <a href={`/post/${p.id}`}>
-                <article class="w-full h-full px-4 py-1 my-1 rounded hover:cursor-pointer">
-                    <header class="flex h-6 items-center">
-                        <div
-                            class="h-6 w-6 rounded-full bg-gray-300 flex items-center justify-center text-white font-black"
-                        >
-                            j/
-                        </div>
-                        <a class="flex text-xs items-center" href={`/post/${p.community}`}
-                            ><span class="px-1 text-gray-700 font-semibold"
-                                >{`/j/${p.community}`}</span
-                            ></a
-                        >
-                        <span class="text-xs text-gray-500">{`${p.created_at}`}</span>
-                    </header>
-                    <h3
-                        class="my-2 font-bold"
-                        style="color: rgb(34, 34, 34); font-size: 18px; line-height: 22px;"
+            <article class="w-full h-full px-4 py-1 my-1 rounded hover:cursor-pointer">
+                <header class="flex h-6 items-center">
+                    <div
+                        class="h-6 w-6 rounded-full bg-gray-300 flex items-center justify-center text-white font-black"
                     >
-                        {p.title}
-                    </h3>
-                    {#if p.content}
-                        <section class="text-sm line-clamp-6">
-                            {@html marked.parseInline(p.content)}
-                        </section>
-                    {/if}
-                    <button
-                        on:click={(e) => e.stopPropagation()}
-                        type="submit"
-                        form="likeForm"
-                        formaction={`/post/${p.id}?/like`}
-                        class="inline-flex my-2 items-center py-1 rounded-full bg-gray-100"
+                        j/
+                    </div>
+                    <a class="flex text-xs items-center" href={`/post/${p.community}`}
+                        ><span class="px-1 text-gray-700 font-semibold"
+                            >{`/j/${p.community}`}</span
+                        ></a
                     >
-                        <Heart fill={p.liked} />
-                        <span class="pr-2 inline-block mx-1 text-sm font-semibold text-gray-700"
-                            >{p.likes}</span
-                        >
-                    </button>
-                </article>
-            </a>
+                    <span class="text-xs text-gray-500">{`${p.created_at}`}</span>
+                </header>
+                <h3
+                    class="my-2 font-bold"
+                    style="color: rgb(34, 34, 34); font-size: 18px; line-height: 22px;"
+                >
+                    <a href={`/post/${p.id}`}>{p.title}</a>
+                </h3>
+                {#if p.content}
+                    <section class="text-sm line-clamp-6">
+                        {@html marked.parseInline(p.content)}
+                    </section>
+                {/if}
+                <button
+                    on:click={(e) => e.stopPropagation()}
+                    type="submit"
+                    form="likeForm"
+                    formaction={`/post/${p.id}?/like`}
+                    class="inline-flex my-2 items-center py-1 rounded-full bg-gray-100"
+                >
+                    <Heart fill={p.liked} />
+                    <span class="pr-2 inline-block mx-1 text-sm font-semibold text-gray-700"
+                        >{p.likes}</span
+                    >
+                </button>
+            </article>
         {/each}
     </main>
 </div>
