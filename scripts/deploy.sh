7c81d73d7f91845f0e01ed463e7d96b2dda7d38d
jonathan earl <jonathan.earl@tylertech.com>
2024-11-23 09:48:31 -0800

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
-IP=blackbox.wte.sh
-PROJECT=jeddit
-REGISTRY=ghcr.io
-REGISTRY_USER=whattheearl
-CONTAINER_NAME=jeddit
-VERSION=latest
+SERVER=wte
+GIT_URL=https://github.com/whattheearl/jeddit
+BUILD_PATH=/root/build/jeddit
+TAG=ghcr.io/whattheearl/jeddit:latest
+APP_PATH=/root/apps/jeddit
 
 green=$(tput setaf 2)
 normal=$(tput sgr0)
 
 function info {
-  printf "${green}\n$1\n\n\n${normal}" 
+  printf "${green}\n$1\n\n${normal}" 
 }
 
 info "BUILDING CONTAINER" 
-docker build . --tag $REGISTRY/$REGISTRY_USER/$CONTAINER_NAME:$VERSION --platform linux/amd64 || exit 1
-
+ssh $SERVER git clone $GIT_URL $BUILD_PATH
+ssh $SERVER cd $BUILD_PATH && git pull
+ssh $SERVER cd $BUILD_PATH && docker build . --tag $TAG || exit 1
 
 info "PUSHING CONTAINER"
-docker push $REGISTRY/$REGISTRY_USER/$CONTAINER_NAME:$VERSION || exit 1
+ssh $SERVER docker push $TAG || exit 1
 
 info "PUSHING ENV"
-ssh $IP mkdir -p jeddit 
-scp .env.prod $IP:jeddit/.env.prod
-scp docker-compose.yml $IP:jeddit/docker-compose.yml
+ssh $SERVER mkdir -p $APP_PATH || exit 1
+scp .env.prod $SERVER:$APP_PATH || exit 1
+scp docker-compose.yml $SERVER:$APP_PATH || exit 1
 
-info "PULLING CONTAINER"
-ssh $IP cd jeddit; docker compose pull; 
-
-info "RESTARTING CONTAINER"
-ssh $IP cd jeddit; docker compose up -d; 
+info "RESTARTING SERVICE"
+ssh $SERVER "docker compose -f $APP_PATH/docker-compose.yml up -d"
